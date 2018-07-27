43aa202bdfa8d7ef82d3f0be163d86a3a40306f9
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-27 10:09:00 -0400

    done with requirements. Will refactor and attempt bonus next

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,23 @@
 class Image {
-  
+  constructor(id, url, name, like_count, comments) {
+      this.id = id
+      this.url = url
+      this.name = name
+      this.like_count = like_count
+      this.comments = comments
+  }
+
+  render() {
+      document.querySelector("#image").src = this.url
+      document.querySelector("#name").innerText = this.name
+      document.querySelector("#likes").innerText = this.like_count
+      let commentList = document.querySelector("#comments")
+      this.comments.forEach(comment => {
+          let commentEl = document.createElement("li")
+          commentEl.innerText = comment.content
+          commentList.appendChild(commentEl)
+      })
+      document.querySelector("#like_button").addEventListener("click", like)
+  }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 13 //Enter your assigned imageId here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-
-
+ 
+  showImage(imageURL)
 
 })
 
+function showImage(imageURL) {
+  fetch(imageURL).then(r => r.json())
+  .then(imageObj => {
+    let newImage = new Image(imageObj.id, imageObj.url, imageObj.name, imageObj.like_count, imageObj.comments)
+    newImage.render()
+    document.querySelector("form").addEventListener("submit", newCommentHandling)
+  })
+}
+
+function like(e) {
+  let oldLikes = parseInt(document.querySelector("#likes").innerText)
+  document.querySelector("#likes").innerText = oldLikes+1 
+  saveLikes()
+}
+
+function saveLikes() {
+  fetch(`https://randopic.herokuapp.com/likes/`, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({image_id: 13})
+  })
+}
+
+function newCommentHandling(e) {
+  e.preventDefault()
+  let newComment = document.createElement("li")
+  let commentText = e.target.querySelector("#comment_input").value
+  newComment.innerText = commentText
+  document.querySelector("#comments").appendChild(newComment)
+  e.target.reset()
+  saveComment(commentText)
+}
+
+function saveComment(commentText) {
+  fetch('https://randopic.herokuapp.com/comments', {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({image_id: 13, content: commentText})
+  }).then(r => r.json()).then(console.log)
+}
